e2a8b0d1d9e23289bd9ec204c3b0969d67775f3d
RAGUL RAJ N <152047129+RagulRajN@users.noreply.github.com>
2026-06-17 09:14:20 +0530

    SumOfPrimeNumbers.java

---
+++ SumOfPrimeNumbers.java
@@ -0,0 +1,32 @@
+public class SumOfPrimeNumbers {
+
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        int num = 2;
+        int sum = 0;
+
+        while (count < 100) {
+            if (isPrime(num)) {
+                sum += num;
+                count++;
+            }
+            num++;
+        }
+
+        System.out.println("Sum of the first 100 prime numbers: " + sum);
+    }
+}
